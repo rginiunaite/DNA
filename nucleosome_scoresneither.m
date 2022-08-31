64ce01b1c474de1030cb160f9a417ce80cd41efc
rginiunaite <rasaginiu@gmail.com>
2022-08-31 15:42:16 +0300

    nucleosome scores
    nucleosome scores for all chromosomes

---
+++ nucleosome_scoresneither.m
@@ -0,0 +1,78 @@
+%% check nucleosome scores in CpG islands and NMIs 
+
+function nucleosome_scoresCpGandnotCpG(chromNr1,chromNr2)
+
+
+% load coordinates CpG
+%filename = sprintf('../SkirmantasData/AllChromosomesSimulations/coordinatesCh1NMI.txt');
+
+for chromNr=chromNr1:chromNr2
+%filename = sprintf('../SkirmantasData/AllChromosomesSimulations/coordinatesCh%iCpGintersectNMI.txt',chromNr);
+filename = sprintf('../SkirmantasData/AllChromosomesSimulations/coordinatesCh%neither.txt',chromNr);
+
+
+%
+coordinatesCpG = load(filename);
+
+% load coordinates notCpG
+%filename = sprintf('../SkirmantasData/AllChromosomesSimulations/coordinatesCh24notCpG.txt');
+
+%coordinatesnotCpG = load(filename);
+
+
+% load score data
+%filename = sprintf('../SkirmantasData/Yazdi2015/H1_dyad_withNOS.bed');
+
+%FID = fopen(filename);
+%data = textscan(FID,'%s');
+%fclose(FID);
+%stringData = string(data{:});
+%'energiesCh1CpG147.txt'
+
+%str_ = reshape(stringData, 4, [])';
+
+%ind1 = str_(:,1) == 'chrY';
+
+%chr1data = str_(ind1,:);
+
+
+
+T = readtable('../SkirmantasData/Yazdi2015/H1_dyad_withNOS.csv');
+%T = readtable('../SkirmantasData/Harwood2019/GSM3312888_iPS_chr1-22_138-161bp.csv','Range', '1:100000');
+%T = readtable('../SkirmantasData/Yazdi2015/chr1_yatsi_H1_dyad_withNOS.csv');
+
+name = sprintf('chr%i',chromNr);
+index = find(strcmp(T.Var1 , name));
+
+indexChstart= index(1);
+
+indexChend =index(end);
+
+
+
+k=1;
+for i =indexChstart:indexChend
+	for j=1:length(coordinatesCpG)-1
+		%% Yatzi:2015
+		if  T.Var2(i)> coordinatesCpG(j,1) && T.Var3(i)<coordinatesCpG(j,2) %CpG islands
+		%if  T.Var2(i)> coordinatesCpG(j,2) && T.Var3(i)<coordinatesCpG(j+1,1) %% not CpG islands	
+		storeCpG(k,1) = T.Var4(i);
+		
+		%% Harwood:2019
+		%if  T.Var2(i)> coordinatesCpG(j,1) && T.Var2(i)<coordinatesCpG(j,2) %CpG islands
+		%if  T.Var2(i)> coordinatesCpG(j,2) && T.Var2(i)<coordinatesCpG(j+1,1) %% not CpG islands	
+		%	storeCpG(k,1) = T.Var3(i);		
+
+			k=k+1;
+			break
+		end
+	end
+end
+
+filename1 = sprintf('data/Yatzi_Harwood/Ch%iYazdineither.txt',chromNr);
+writematrix(storeCpG,filename1)
+
+end
+
+end
+
